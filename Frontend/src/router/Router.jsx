a95ddca96f4f11cb457223ea28e3f30c968801fd
YashikaRupasinghe <yashika.rupasinghe@gmail.com>
2025-03-10 22:11:21 +0530

    domain page paths were added to Router

--- Frontend/src/router/Router.jsx
+++ Frontend/src/router/Router.jsx
@@ -4,6 +4,11 @@ import Register from "../pages/Register";
 import Landing from "../pages/LandingPage";
 import Home from "../pages/Home";
 
+import CognitiveQuestions from "../pages/domains/CognitiveQuestions";
+import AffectiveQuestion from "../pages/domains/AffectiveQuestions";
+import MetaCognitiveDomain from "../pages/domains/MetaCognitiveDomain";
+import PsycomotorQuestions from "../pages/domains/PsycomotorQuestions";
+
 // import Requirements from "../pages/Content/Requirement";
 import { AuthGuard, ProtectedRoute } from "../guards/Authguard";
 import Dashboard from "../pages/Dashboard";
@@ -48,6 +53,38 @@ const router = createBrowserRouter([
         element: (
           <ProtectedRoute>
             <Dashboard />
+          </ProtectedRoute>
+        ),
+    },
+    {
+        path: "/cognitive-questions",
+        element: (
+          <ProtectedRoute>
+            <CognitiveQuestions />
+          </ProtectedRoute>
+        ),
+    },
+    {
+        path: "/affective-questions",
+        element: (
+          <ProtectedRoute>
+            <AffectiveQuestion />
+          </ProtectedRoute>
+        ),
+    },
+    {
+        path: "/meta-cognitive-questions",
+        element: (
+          <ProtectedRoute>
+            <MetaCognitiveDomain />
+          </ProtectedRoute>
+        ),
+    },
+    {
+        path: "/psycomotor-questions",
+        element: (
+          <ProtectedRoute>
+            <PsycomotorQuestions />
           </ProtectedRoute>
         ),
     },
