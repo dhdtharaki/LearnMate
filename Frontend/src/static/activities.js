096b7d68dfa82d67604c37b99c9485179de55997
PehesaraniWKA <it21259470@my.sliit.lk>
2025-03-11 20:01:08 +0530

    Added activities for cognitive domain

--- Frontend/src/static/activities.js
+++ Frontend/src/static/activities.js
@@ -14,6 +14,22 @@ const activities = [
     { name: "Practice recognizing and discussing feelings with simple role-play exercises", timeTake: 17, retries: 2 },
     { name: "Work on simple puzzles that require sequential thinking and problem-solving", timeTake: 14, retries: 2 },
     { name: "Use role-play to practice recognizing mistakes and discussing what can be improved", timeTake: 20, retries: 2 },
+    { name: "Use role-play to practice recognizing mistakes and discussing what can be improved", timeTake: 20, retries: 2 },
+    { name: "Organize chores into smaller tasks and track progress", timeTake: 18, retries: 2 },
+    { name: "Set simple goals and break them down into smaller achievable steps", timeTake: 21, retries: 2 },
+    { name: "Use guided discussions to set and identify personal goals for the future", timeTake: 27, retries: 2 },
+    { name: "Work on problem-solving puzzles that require recognizing patterns and strategies", timeTake: 32, retries: 2 },
+    { name: "Practice organizing tasks by priority with a simple list or visual chart", timeTake: 23, retries: 2 },
+    { name: "Work one-on-one with a coach to break down tasks into simple steps", timeTake: 16, retries: 3 },
+    { name: "Practice social skills through role-play exercises that focus on recognition of mistakes", timeTake: 20, retries: 3 },
+    { name: "Engage in short focused activities like simple puzzles or memory games", timeTake: 17, retries: 3 },
+    { name: "Use visual aids and cues to improve social interaction and task completion", timeTake: 24, retries: 3 },
+    { name: "Practice memory games where steps must be recalled in order", timeTake: 22, retries: 3 },
+    { name: "Provide immediate feedback during tasks to reinforce step completion", timeTake: 26, retries: 3 },
+    { name: "Gradually introduce tasks with multiple steps and provide clear instructions", timeTake: 29, retries: 3 },
+    { name: "Practice task prioritization with simple guidance like a checklist or reminder", timeTake: 25, retries: 3 },
+    { name: "Work on task prioritization without external help by using visual aids and reminders", timeTake: 32, retries: 3 },
+    { name: "Role-playing Social Scenarios", timeTake: 10, retries: 1 },
 ];
 
 function findActivityByName(name) {
